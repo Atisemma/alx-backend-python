16c2f990245cda6d735c335d22b290e22d407c8b
Atisemma <atisemma26@gmail.com>
2024-07-11 22:22:44 +0000

    Return accepted data type

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module that provides a function to calculate the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Calculate and return the floor of a given float number.
+
+    Args:
+        n (float): The float number to calculate the floor of.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
